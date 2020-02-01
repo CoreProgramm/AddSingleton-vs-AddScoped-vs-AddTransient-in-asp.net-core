e9bb0900beb9d7b0e11041d23eb10ddf4b1cb3bc
JT <coreprogramm@gmail.com>
2020-02-02 00:58:34 +0530

    Difference between AddSingleton vs AddScoped vs AddTransient in asp.net core
    Difference between AddSingleton vs AddScoped vs AddTransient in asp.net core

---
+++ dependancyInjection/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dependancyInjection.Models;
+
+namespace dependancyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IMobileService _mobileService;
+        public HomeController(ILogger<HomeController> logger, IMobileService mobileService)
+        {
+            _logger = logger;
+            _mobileService = mobileService;
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Mobile mobile)
+        {
+            if (ModelState.IsValid)
+            {
+                Mobile newMobile = _mobileService.Add(mobile);
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ dependancyInjection/Models/IMobileService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dependancyInjection.Models
+{
+   public interface IMobileService
+    {
+        IEnumerable<Mobile> GetAll();   
+        Mobile Add(Mobile mobile);
+    }
+}
